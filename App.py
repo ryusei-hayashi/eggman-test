98ba9f35840a3ef2b6ea53693304a40deefbe38b
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2025-01-22 14:45:18 +0900

    Update App.py

--- App.py
+++ App.py
@@ -101,19 +101,19 @@ def music(t, m):
 def scene(c, s):
     with c:
         st.header(s)
-        u = st.multiselect(f'State of {s}', ('オープニング', 'エンディング', 'タイトル', 'イベント', 'チュートリアル', 'メニュー画面', 'ショップ画面', 'マップ画面', 'ゲーム失敗', 'ゲーム成功', 'ハイライト', 'アナウンス', 'マイルーム', 'フィールド', 'ダンジョン', 'ステージ'), key=f'State_of_{s}', placeholder='オープニング/ダンジョン/...')
-        t = st.multiselect(f'Time of {s}', ('春', '夏', '秋', '冬', '朝', '昼', '夜', '明方', '夕方', '休日', '原始', '古代', '中世', '近代', '現代', '未来'), key=f'Time_of_{s}', placeholder='春/朝/...')
-        w = st.multiselect(f'Weather of {s}', ('星', '虹', '晴れ', '曇り', '霧', '砂', '雪', '雷', '雨', '小雨', '大雨', '突風', '混沌', '朝焼け', '夕焼け', 'オーロラ'), key=f'Weather_of_{s}', placeholder='晴れ/曇り/...')
-        b = st.multiselect(f'Biome of {s}', ('異次元', '虚無', '宇宙', '大陸', '海', '空', '東', '西', '南', '北', '氷', '炎', '花', '毒', '沼', '湖', '泉', '滝', '川', '島', '岩', '崖', '山岳', '峡谷', '洞窟', '温泉', '水中', '水辺', '岸辺', '浜辺', '砂漠', '荒野', '草原', '森林', 'サバンナ', 'ジャングル'), key=f'Biome_of_{s}', placeholder='草原/砂漠/...')
-        p = st.multiselect(f'Place of {s}', ('仮想空間', '都会', '田舎', '街', '村', '橋', '道路', '路地裏', '地下道', '航空機', '鉄道', '船', '港', '駅', '店', '墓', '寺', '神社', '教会', '公園', '学校', '法廷', '病院', '劇場', '式場', '競技場', '博物館', '動物園', '遊園地', '飲み屋', '宿泊施設', '研究機関', '軍事基地', '城塞', '牢獄', '倉庫', '工場', '牧場', '畑', '庭', '家', '邸宅', '廃屋', '遺跡', '宮殿', '神殿', 'ロビー', 'タワー', 'ビル', 'ジム', 'プール', 'エステ', 'カフェ', 'レストラン', 'リゾート', 'オフィス', 'アジト', 'カジノ', 'キャバクラ', 'ナイトクラブ'), key=f'Place_of_{s}', placeholder='街/店/...')
-        q = st.multiselect(f'Person of {s}', ('ゆるキャラ', 'ヒーロー', 'ヒロイン', 'スパイ', 'ライバル', 'ラスボス', 'ボス', 'モブ', '大衆', '貴族', '偉人', '仲間', '孤独', '平穏', '不穏', '敵'), key=f'Person_of_{s}', placeholder='ヒロイン/ラスボス/...')
-        a = st.multiselect(f'Action of {s}', ('ギャグ', 'ギャンブル', 'スポーツ', 'パーティー', 'レース', '悪事', '泳ぐ', '会話', '回想', '葛藤', '感動', '危機', '祈り', '儀式', '議論', '休憩', '結婚', '公演', '好機', '錯乱', '仕事', '支度', '失意', '集まる', '祝福', '出会う', '出掛ける', '勝利', '食事', '声援', '説明', '戦い', '潜入', '走る', '騒動', '探検', '仲違い', '仲直り', '挑戦', '挑発', '追跡', '登場', '逃走', '謎解き', '破滅', '敗北', '買い物', '犯罪', '飛行', '不倫', '復讐', '奮起', '別れ', '謀略', '癒し', '誘惑', '遊ぶ', '恋愛', '練習', '騙す'), key=f'Action_of_{s}', placeholder='...')
+        u = st.multiselect(f'State of {s}', ('オープニング', 'エンディング', 'タイトル', 'イベント', 'チュートリアル', 'メニュー画面', 'ショップ画面', 'マップ画面', 'ゲーム失敗', 'ゲーム成功', 'ハイライト', 'アナウンス', 'マイルーム', 'フィールド', 'ダンジョン', 'ステージ'), key=f'State of {s}', placeholder='オープニング/ダンジョン/...')
+        t = st.multiselect(f'Time of {s}', ('春', '夏', '秋', '冬', '朝', '昼', '夜', '明方', '夕方', '休日', '原始', '古代', '中世', '近代', '現代', '未来'), key=f'Time of {s}', placeholder='春/朝/...')
+        w = st.multiselect(f'Weather of {s}', ('星', '虹', '晴れ', '曇り', '霧', '砂', '雪', '雷', '雨', '小雨', '大雨', '突風', '混沌', '朝焼け', '夕焼け', 'オーロラ'), key=f'Weather of {s}', placeholder='晴れ/曇り/...')
+        b = st.multiselect(f'Biome of {s}', ('異次元', '虚無', '宇宙', '大陸', '海', '空', '東', '西', '南', '北', '氷', '炎', '花', '毒', '沼', '湖', '泉', '滝', '川', '島', '岩', '崖', '山岳', '峡谷', '洞窟', '温泉', '水中', '水辺', '岸辺', '浜辺', '砂漠', '荒野', '草原', '森林', 'サバンナ', 'ジャングル'), key=f'Biome of {s}', placeholder='草原/砂漠/...')
+        p = st.multiselect(f'Place of {s}', ('仮想空間', '都会', '田舎', '街', '村', '橋', '道路', '路地裏', '地下道', '航空機', '鉄道', '船', '港', '駅', '店', '墓', '寺', '神社', '教会', '公園', '学校', '法廷', '病院', '劇場', '式場', '競技場', '博物館', '動物園', '遊園地', '飲み屋', '宿泊施設', '研究機関', '軍事基地', '城塞', '牢獄', '倉庫', '工場', '牧場', '畑', '庭', '家', '邸宅', '廃屋', '遺跡', '宮殿', '神殿', 'ロビー', 'タワー', 'ビル', 'ジム', 'プール', 'エステ', 'カフェ', 'レストラン', 'リゾート', 'オフィス', 'アジト', 'カジノ', 'キャバクラ', 'ナイトクラブ'), key=f'Place of {s}', placeholder='街/店/...')
+        q = st.multiselect(f'Person of {s}', ('ゆるキャラ', 'ヒーロー', 'ヒロイン', 'スパイ', 'ライバル', 'ラスボス', 'ボス', 'モブ', '大衆', '貴族', '偉人', '仲間', '孤独', '平穏', '不穏', '敵'), key=f'Person of {s}', placeholder='ヒロイン/ラスボス/...')
+        a = st.multiselect(f'Action of {s}', ('ギャグ', 'ギャンブル', 'スポーツ', 'パーティー', 'レース', '悪事', '泳ぐ', '会話', '回想', '葛藤', '感動', '危機', '祈り', '儀式', '議論', '休憩', '結婚', '公演', '好機', '錯乱', '仕事', '支度', '失意', '集まる', '祝福', '出会う', '出掛ける', '勝利', '食事', '声援', '説明', '戦い', '潜入', '走る', '騒動', '探検', '仲違い', '仲直り', '挑戦', '挑発', '追跡', '登場', '逃走', '謎解き', '破滅', '敗北', '買い物', '犯罪', '飛行', '不倫', '復讐', '奮起', '別れ', '謀略', '癒し', '誘惑', '遊ぶ', '恋愛', '練習', '騙す'), key=f'Action of {s}', placeholder='...')
         st.subheader(f'Mood of {s}')
         l, r = st.columns(2, gap='medium')
         with l:
-            v = st.slider(f'Valence of {s}', -1.0, 1.0, (-1.0, 1.0), key=f'Valence_of_{s}')
+            v = st.slider(f'Valence of {s}', -1.0, 1.0, (-1.0, 1.0), key=f'Valence of {s}')
         with r:
-            z = st.slider(f'Arousal of {s}', -1.0, 1.0, (-1.0, 1.0), key=f'Arousal_of_{s}')
+            z = st.slider(f'Arousal of {s}', -1.0, 1.0, (-1.0, 1.0), key=f'Arousal of {s}')
     return T['scn'].map(lambda i: set(u + t + w + b + p + q + a).issubset(i)) & T['pn'].between(v[0], v[1]) & T['ap'].between(z[0], z[1])
 
 def idx(n, v):
@@ -150,13 +150,13 @@ bin = 1025
 M = model('data/model.pkl')
 T, a, b = table('data/table.pkl')
 
-if st.text_input('Your Name', key='Your_Name'):
+if st.text_input('Your Name', key='Your Name'):
     st.image('imgs/logo.png')
     st.markdown('EgGMAn (Engine of Game Music Analogy) search for game music considering game and scene feature at the same time')
     
     st.header('Source Music')
-    s = st.segmented_control('Type of Source Music', ('Web Service', 'Direct Link', 'Audio File'), default='Web Service', key='Type_of_Source_Music')
-    y = music(s, st.file_uploader('File of Source Music', key='File_of_Source_Music') if 'File' in s else st.text_input('URL of Source Music', key='URL_of_Source_Music'))
+    s = st.segmented_control('Type of Source Music', ('Web Service', 'Direct Link', 'Audio File'), default='Web Service', key='Type of Source Music')
+    y = music(s, st.file_uploader('File of Source Music', key='File of Source Music') if 'File' in s else st.text_input('URL of Source Music', key='URL of Source Music'))
     
     c = st.columns(2, gap='large')
     p = scene(c[0], 'Source Scene')
@@ -164,10 +164,10 @@ if st.text_input('Your Name', key='Your_Name'):
     
     st.header('Target Music')
     with st.popover('Search Option'):
-        i = st.multiselect('Ignore Artist', ('ANDY', 'BGMer', 'Nash Music Library', 'Seiko', 'TAZ', 'hitoshi', 'zukisuzuki', 'たう', 'ガレトコ', 'ユーフルカ'), key='Ignore_Artist', placeholder='')
-        j = st.multiselect('Ignore Site', ('BGMer', 'BGMusic', 'Nash Music Library', 'PeriTune', 'Senses Circuit', 'zukisuzuki BGM', 'ガレトコ', 'ユーフルカ', '音の園'), key='Ignore_Site', placeholder='')
-        t = st.slider('Time Range', dt.time(0), dt.time(1), (dt.time(0), dt.time(1)), dt.timedelta(seconds=10), 'mm:ss', key='Time_Range')
-        r = st.slider('Random Rate', 0.0, 1.0, 1.0, key='Random_Rate')
+        i = st.multiselect('Ignore Artist', ('ANDY', 'BGMer', 'Nash Music Library', 'Seiko', 'TAZ', 'hitoshi', 'zukisuzuki', 'たう', 'ガレトコ', 'ユーフルカ'), key='Ignore Artist', placeholder='')
+        j = st.multiselect('Ignore Site', ('BGMer', 'BGMusic', 'Nash Music Library', 'PeriTune', 'Senses Circuit', 'zukisuzuki BGM', 'ガレトコ', 'ユーフルカ', '音の園'), key='Ignore Site', placeholder='')
+        t = st.slider('Time Range', dt.time(0), dt.time(1), (dt.time(0), dt.time(1)), dt.timedelta(seconds=10), 'mm:ss', key='Time Range')
+        r = st.slider('Random Rate', 0.0, 1.0, 1.0, key='Random Rate')
     if st.button(f'Search {"EgGMAn" if y.size else "Random"}', type='primary'):
         try:
             if y.size:
